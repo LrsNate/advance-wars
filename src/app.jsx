472cfbf270d4f5ee410e0f2b5d21a2a9be613e91
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-02-03 22:58:12 +0000

    Install React and ESLint

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,9 @@
-console.log('Test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>It works!</h1>;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container'),
+);
