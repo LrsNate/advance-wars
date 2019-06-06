2fd759e8a1fe4234d1666e89ef7d38ecc4ec0228
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2019-06-05 23:00:36 -0700

    Add context menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
 import produce from 'immer';
+import React from 'react';
+import { Overlay, Popover, ListGroup } from 'react-bootstrap';
 
 class Cell extends React.Component {
+  state = { ref: null };
+
+  attachRef = ref => this.setState({ ref });
+
   render() {
-    const { onClick, cell } = this.props;
+    const { onAction, onClick, cell } = this.props;
+    const { ref } = this.state;
+
     const classes = ['aw-map-cell'];
 
     if (cell.selected) {
@@ -16,7 +23,35 @@ class Cell extends React.Component {
       classes.push('in-range');
     }
 
-    return <div className={classes.join(' ')} onClick={onClick} />;
+    return (
+      <React.Fragment>
+        <div
+          ref={this.attachRef}
+          id={`cell-${cell.y}-${cell.x}`}
+          className={classes.join(' ')}
+          onClick={onClick}
+        />
+        <Overlay target={ref} show={!!cell.actions} placement="bottom">
+          {({ show, ...props }) => (
+            <Popover {...props}>
+              <ListGroup variant="flush">
+                {cell.actions &&
+                  Object.entries(cell.actions).map(([action, label]) => (
+                    <ListGroup.Item
+                      action
+                      as="li"
+                      key={action}
+                      onClick={() => onAction(action)}
+                    >
+                      {label}
+                    </ListGroup.Item>
+                  ))}
+              </ListGroup>
+            </Popover>
+          )}
+        </Overlay>
+      </React.Fragment>
+    );
   }
 }
 
@@ -34,18 +69,23 @@ function makeMap(rows, cols) {
   return result;
 }
 
-function addUnit(map, unit) {
-  return produce(map, draft => {
-    draft[unit.y][unit.x].unit = unit;
-  });
-}
-
 function getSelected(map) {
   for (let y = 0; y < map.length; y++) {
     const row = map[y];
     for (let x = 0; x < row.length; x++) {
       const cell = row[x];
       if (cell.selected) return cell;
+    }
+  }
+  return null;
+}
+
+function getTarget(map) {
+  for (let y = 0; y < map.length; y++) {
+    const row = map[y];
+    for (let x = 0; x < row.length; x++) {
+      const cell = row[x];
+      if (cell.actions) return cell;
     }
   }
   return null;
@@ -71,6 +111,7 @@ function resetMap(map) {
     row.forEach(cell => {
       cell.selected = false;
       cell.inRange = false;
+      cell.actions = null;
     });
   });
 }
@@ -88,6 +129,7 @@ function selectUnit(map, x, y) {
 }
 
 function moveUnit(src, dest) {
+  if (dest.unit) return;
   const { unit } = src;
   src.unit = null;
 
@@ -97,7 +139,16 @@ function moveUnit(src, dest) {
 }
 
 class App extends React.Component {
-  state = { map: addUnit(makeMap(7, 7), { type: 'mech', x: 2, y: 3 }) };
+  state = { map: makeMap(7, 7) };
+
+  componentDidMount() {
+    this.setState(
+      produce(({ map }) => {
+        map[3][2].unit = { type: 'mech', x: 2, y: 3 };
+        map[5][5].unit = { type: 'mech', x: 5, y: 5 };
+      })
+    );
+  }
 
   handleClick(x, y) {
     this.setState(
@@ -107,14 +158,31 @@ class App extends React.Component {
           selectCell(map, x, y);
         } else {
           const cell = map[y][x];
+
           if (cell.inRange) {
-            moveUnit(selected, cell);
+            cell.actions = { move: 'Move', cancel: 'Cancel' };
+          } else {
+            resetMap(map);
           }
-          resetMap(map);
         }
       })
     );
   }
+
+  handleAction = action => {
+    this.setState(
+      produce(({ map }) => {
+        const selected = getSelected(map);
+        const target = getTarget(map);
+        console.log(selected, target);
+
+        if (action === 'move') {
+          moveUnit(selected, target);
+        }
+        resetMap(map);
+      })
+    );
+  };
 
   render() {
     const { map } = this.state;
@@ -127,6 +195,7 @@ class App extends React.Component {
                 cell={cell}
                 key={`${cell.y},${cell.x}`}
                 onClick={() => this.handleClick(cell.x, cell.y)}
+                onAction={this.handleAction}
               />
             ))}
           </div>
